b50619c62fe545f9d98696fcb540d38c8e0316fe
francisco <correo@gmail.com>
2023-03-30 19:25:46 +0200

    Nuevo test decelerar

--- Test/TestCoche.java
+++ Test/TestCoche.java
@@ -13,4 +13,11 @@ public class TestCoche {
         nuevoCoche.acelerar(30);
        Assertions.assertEquals(30,nuevoCoche.velocidad);
     }
+    @Test
+    public void test_decelerar_un_coche_su_velocidad_disminuye(){
+        Coche nuevoCoche = new Coche();
+        nuevoCoche.velocidad=50;
+        nuevoCoche.decelerar(30);
+        Assertions.assertEquals(30,nuevoCoche.velocidad);
+    }
 }
