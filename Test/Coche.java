5db47f504a5183b277f55ed0f60baa7cae02baf6
francisco <correo@gmail.com>
2023-03-30 19:37:44 +0200

    Refactorizando los metodos

--- Test/Coche.java
+++ Test/Coche.java
@@ -1,11 +1,11 @@
 public class Coche {
     public int velocidad;
 
-    public void acelerar(int aceleracion) {
+    public void acelerar_Francisco_andres_garcia_garcia(int aceleracion) {
         velocidad += aceleracion;
     }
 
-    public void decelerar(int deceleracion) {
+    public void decelerar_Francisco_andres_garcia_garcia(int deceleracion) {
         velocidad -= deceleracion;
         if(velocidad<0)velocidad=0;
     }
--- Test/TestCoche.java
+++ Test/TestCoche.java
@@ -10,21 +10,21 @@ public class TestCoche {
     @Test
     public void test_acelerar_un_coche_su_velocidad_aumenta(){
         Coche nuevoCoche = new Coche();
-        nuevoCoche.acelerar(30);
+        nuevoCoche.acelerar_Francisco_andres_garcia_garcia(30);
        Assertions.assertEquals(30,nuevoCoche.velocidad);
     }
     @Test
     public void test_decelerar_un_coche_su_velocidad_disminuye(){
         Coche nuevoCoche = new Coche();
         nuevoCoche.velocidad=50;
-        nuevoCoche.decelerar(20);
+        nuevoCoche.decelerar_Francisco_andres_garcia_garcia(20);
         Assertions.assertEquals(30,nuevoCoche.velocidad);
     }
     @Test
     public void test_decelerar_un_coche_su_velocidad_no_puede_ser_menos_que_cero(){
         Coche nuevoCoche = new Coche();
         nuevoCoche.velocidad=50;
-        nuevoCoche.decelerar(80);
+        nuevoCoche.decelerar_Francisco_andres_garcia_garcia(80);
         Assertions.assertEquals(0,nuevoCoche.velocidad);
     }
 }
